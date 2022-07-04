f7a81f65e9b444f01bdf15013dcecb8712ad229e
Henry001-prog <alissonhenrique001@hotmail.com.br>
2022-07-04 19:48:50 -0300

    new package and yarnlock

--- src/pages/LoginScreen/index.js
+++ src/pages/LoginScreen/index.js
@@ -1,100 +1,101 @@
-import React, { useEffect, useState } from 'react';
-import { Keyboard } from 'react-native';
-import firebase from 'firebase';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { Keyboard } from "react-native";
+import firebase from "firebase";
+import { connect } from "react-redux";
 
-import { tryLogin } from '../../store/actions';
-import { useSelector, useDispatch } from 'react-redux';
+import { tryLogin } from "../../store/actions";
+import { useSelector, useDispatch } from "react-redux";
 
-import { 
-    Div, 
-    Form, 
-    Input, 
-    Loading, 
-    Button, 
-    TextButton,
-    ViewErrorMessage, 
-    ErrorMessage 
-} from './styles';
+import {
+  Div,
+  Form,
+  Input,
+  Loading,
+  Button,
+  TextButton,
+  ViewErrorMessage,
+  ErrorMessage,
+} from "./styles";
 
-import {showMessage} from "react-native-flash-message";
+import { showMessage } from "react-native-flash-message";
 
-export default function LoginPage({ navigation} ) {
+export default function LoginPage({ navigation }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  //console.log('email:', email);
+  const [password, setPassword] = useState("");
+  //console.log('password:', password);
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [message, setMessage] = useState('');
-    const [email, setEmail] = useState('');
-    //console.log('email:', email);
-    const [password, setPassword] = useState('');
-    //console.log('password:', password);
-    
-    const user = useSelector((state) => state.user);
-    const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const firebaseConfig = {
-            apiKey: "AIzaSyCsNst_gYIg6AUwqqx-YVRZepywsNL_xI0",
-            authDomain: "series-b92be.firebaseapp.com",
-            databaseURL: "https://series-b92be.firebaseio.com",
-            projectId: "series-b92be",
-            storageBucket: "series-b92be.appspot.com",
-            messagingSenderId: "828328217426",
-            appId: "1:828328217426:web:52c7133173d11a64a3888b",
-            measurementId: "G-EP4S5RF9SK"
-        };
-          // Initialize Firebase
-        if (!firebase.apps.length) {
-            firebase.initializeApp(firebaseConfig);
-        }
-    }, []);
-    
-
-    function getMessageByErrorCode(errorCode) {
-        switch (errorCode) {
-            case 'auth/wrong-password':
-                return 'Senha incorreta';
-            case 'auth/user-not-found':
-                return 'Usuário não encontrado';
-            default:
-                return 'Erro desconhecido';
-        }
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCsNst_gYIg6AUwqqx-YVRZepywsNL_xI0",
+      authDomain: "series-b92be.firebaseapp.com",
+      databaseURL: "https://series-b92be.firebaseio.com",
+      projectId: "series-b92be",
+      storageBucket: "series-b92be.appspot.com",
+      messagingSenderId: "828328217426",
+      appId: "1:828328217426:web:52c7133173d11a64a3888b",
+      measurementId: "G-EP4S5RF9SK",
+    };
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
     }
+  }, []);
 
-    function renderButton() {
-        if (isLoading)
-            return <Loading size='large' color='light-blue'/>;
-        return (
-            <Button 
-                title='Entrar' 
-                onPress={() => {dispatch(tryLogin(email, password, navigation, setIsLoading)); Keyboard.dismiss();}}>
-                    <TextButton>Entrar</TextButton>
-                </Button>
-        );
+  function getMessageByErrorCode(errorCode) {
+    switch (errorCode) {
+      case "auth/wrong-password":
+        return "Senha incorreta";
+      case "auth/user-not-found":
+        return "Usuário não encontrado";
+      default:
+        return "Erro desconhecido";
     }
+  }
 
+  function renderButton() {
+    if (isLoading) return <Loading size="large" color="light-blue" />;
     return (
-        <Div>
-            <Form first>
-                <Input 
-                    placeholder="user@email.com"
-                    placeholderTextColor= '#DCDCDC'
-                    value={email}
-                    onChangeText={(value) => setEmail(value)}
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                />
-            </Form>
-            <Form last>
-                <Input
-                    placeholder="******"
-                    placeholderTextColor= '#DCDCDC' 
-                    secureTextEntry
-                    value={password}
-                    onChangeText={(value) => setPassword(value)}
-                />
-            </Form>
-
-            {renderButton()}
-        </Div>
+      <Button
+        title="Entrar"
+        onPress={() => {
+          dispatch(tryLogin(email, password, navigation, setIsLoading));
+          Keyboard.dismiss();
+        }}
+      >
+        <TextButton>Entrar</TextButton>
+      </Button>
     );
+  }
+
+  return (
+    <Div>
+      <Form first>
+        <Input
+          placeholder="user@email.com"
+          placeholderTextColor="#DCDCDC"
+          value={email}
+          onChangeText={(value) => setEmail(value)}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+      </Form>
+      <Form last>
+        <Input
+          placeholder="******"
+          placeholderTextColor="#DCDCDC"
+          secureTextEntry
+          value={password}
+          onChangeText={(value) => setPassword(value)}
+        />
+      </Form>
+
+      {renderButton()}
+    </Div>
+  );
 }
