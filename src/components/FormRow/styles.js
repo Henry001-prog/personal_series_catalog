504f91d560aa429d68396eeae74a84d9c4287ee7
Henry001-prog <alissonhenrique001@hotmail.com.br>
2022-07-27 02:28:53 -0300

    typescript-added-in-project

--- src/components/FormRow/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-
-export const Container = styled.View.attrs({})`
-  width: 100%;
-  background-color: transparent;
-  margin: 5px 0px 5px 0px;
-  margin-top: ${(props) => (props.first ? "50px" : "15px")};
-  margin-bottom: ${(props) => (props.last ? "10px" : "5px")};
-`;
--- src/components/Header/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components/native";
-import { BorderlessButton } from "react-native-gesture-handler";
-import Icon from "react-native-vector-icons/MaterialIcons";
-
-export const BoxIcon = styled.TouchableOpacity`
-  margin-top: 40px;
-  align-items: flex-start;
-  margin-left: 20px;
-`;
